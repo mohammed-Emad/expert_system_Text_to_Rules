e9a3f550c2f0fd676622f12944522baa8b2a2295
mohammed-Emad <mohammed.imad38@yahoo.com>
2018-07-19 04:14:15 +0200

    Translated knowledge

---
+++ compiled_knowledge.py
@@ -0,0 +1,434 @@
+#Written on 19 July 2018 by Mohamed Emad
+#This was set up in January of the same year
+
+from pyknow import *
+
+class engine(KnowledgeEngine):
+
+	@Rule(Fact(temperature = 50))
+	def fact_P0(self):
+		heater = 300
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 156))
+	def fact_P1(self):
+		heater = 1221
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 36))
+	def fact_P2(self):
+		heater = 820
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 129))
+	def fact_P3(self):
+		heater = 115
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 166))
+	def fact_P4(self):
+		heater = 1124
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 136))
+	def fact_P5(self):
+		heater = 776
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 88))
+	def fact_P6(self):
+		heater = 409
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 148))
+	def fact_P7(self):
+		heater = 264
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 192))
+	def fact_P8(self):
+		heater = 925
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 20))
+	def fact_P9(self):
+		heater = 574
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 64))
+	def fact_P10(self):
+		heater = 176
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 96))
+	def fact_P11(self):
+		heater = 511
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 200))
+	def fact_P12(self):
+		heater = 933
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 69))
+	def fact_P13(self):
+		heater = 408
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 81))
+	def fact_P14(self):
+		heater = 1247
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 161))
+	def fact_P15(self):
+		heater = 414
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 104))
+	def fact_P16(self):
+		heater = 447
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 72))
+	def fact_P17(self):
+		heater = 1221
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 108))
+	def fact_P18(self):
+		heater = 299
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 159))
+	def fact_P19(self):
+		heater = 248
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 26))
+	def fact_P20(self):
+		heater = 758
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 68))
+	def fact_P21(self):
+		heater = 88
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 65))
+	def fact_P22(self):
+		heater = 1089
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 59))
+	def fact_P23(self):
+		heater = 683
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 107))
+	def fact_P24(self):
+		heater = 374
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 44))
+	def fact_P25(self):
+		heater = 694
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 156))
+	def fact_P26(self):
+		heater = 298
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 83))
+	def fact_P27(self):
+		heater = 689
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 115))
+	def fact_P28(self):
+		heater = 828
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 51))
+	def fact_P29(self):
+		heater = 870
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 11))
+	def fact_P30(self):
+		heater = 394
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 153))
+	def fact_P31(self):
+		heater = 101
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 110))
+	def fact_P32(self):
+		heater = 1107
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 144))
+	def fact_P33(self):
+		heater = 353
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 157))
+	def fact_P34(self):
+		heater = 1291
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 37))
+	def fact_P35(self):
+		heater = 942
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 109))
+	def fact_P36(self):
+		heater = 1060
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 38))
+	def fact_P37(self):
+		heater = 146
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 178))
+	def fact_P38(self):
+		heater = 1075
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 22))
+	def fact_P39(self):
+		heater = 371
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 38))
+	def fact_P40(self):
+		heater = 362
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 90))
+	def fact_P41(self):
+		heater = 1068
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 69))
+	def fact_P42(self):
+		heater = 575
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 33))
+	def fact_P43(self):
+		heater = 634
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 133))
+	def fact_P44(self):
+		heater = 574
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 107))
+	def fact_P45(self):
+		heater = 184
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 154))
+	def fact_P46(self):
+		heater = 1283
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 170))
+	def fact_P47(self):
+		heater = 1087
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 183))
+	def fact_P48(self):
+		heater = 164
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 156))
+	def fact_P49(self):
+		heater = 884
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 81))
+	def fact_P50(self):
+		heater = 506
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 123))
+	def fact_P51(self):
+		heater = 349
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 104))
+	def fact_P52(self):
+		heater = 1116
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 32))
+	def fact_P53(self):
+		heater = 148
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 177))
+	def fact_P54(self):
+		heater = 138
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 134))
+	def fact_P55(self):
+		heater = 937
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 165))
+	def fact_P56(self):
+		heater = 866
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 53))
+	def fact_P57(self):
+		heater = 935
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 136))
+	def fact_P58(self):
+		heater = 520
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 93))
+	def fact_P59(self):
+		heater = 206
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
+	@Rule(Fact(temperature = 49))
+	def fact_P60(self):
+		heater = 609
+		print('heater :=', heater)
+		''' Here write the rest of your operations and orders '''
+
+
